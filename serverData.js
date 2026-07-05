5f4b652dae36f97cb7ae7a3f945827bbac57af01
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-05 10:32:50 +0300

    fix func to write to json file

--- serverData.js
+++ serverData.js
@@ -1,10 +1,20 @@
+import fs from "fs/promises";
+import { get } from "http";
+import { constrainedMemory } from "process";
+
 const url = "https://server-for-test-week-13.onrender.com/api/race";
 
 export async function getData() {
     console.log("Loading queue data...");
-    let response = await fetch(url);
-    let data = await response.json();
-    setTimeout(() => {
-        return data;
-    }, 1500);
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        setTimeout(() => {
+            fs.writeFile("./data.json", JSON.stringify(data, null, 4), "utf8");
+        }, 1500);
+    } catch (err) {
+        console.log(`Error: ${err}`);
+    }
 }
+
+getData();
