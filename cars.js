b0be5033a634bd7cc86b398a11c02820533391ed
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-05 11:53:18 +0300

    added searchCar func

--- cars.js
+++ cars.js
@@ -5,3 +5,17 @@ export function carsData() {
     let cars = data.cars;
     return cars;
 }
+
+export function searchCar(carNumber, cars) {
+    let car = cars.find((car) => car.carNumber === carNumber);
+    console.log(car);
+    if (!car) {
+        console.log(
+            `Error: No car found with number #${carNumber} in the current race.`,
+        );
+    } else {
+        console.log(
+            `Found car #${carNumber}| Driver: ${car.driverName} | Status: ${car.status}`,
+        );
+    }
+}
